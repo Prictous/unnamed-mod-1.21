8fae47291fd62f704d02cf11943ccf33173c779e
Prictous <thepotto1@gmail.com>
2024-09-07 16:54:48 -0500

    initial commit

---
+++ src/main/java/net/quilters/unnamedmod/UnnamedModClient.java
@@ -0,0 +1,10 @@
+package net.quilters.unnamedmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnnamedModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
